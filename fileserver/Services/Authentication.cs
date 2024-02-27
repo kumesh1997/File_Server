ea5acb89310b24e641c25e831dd93c21c955dbe2
kumesh1997 <tharindukumesh09@gmail.com>
2024-02-27 22:11:43 +0530

    mvp 1

---
+++ fileserver/Services/Authentication.cs
@@ -0,0 +1,202 @@
+﻿using Amazon;
+using Amazon.CognitoIdentityProvider;
+using Amazon.CognitoIdentityProvider.Model;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Extensions.CognitoAuthentication;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Runtime;
+using fileserver.DTO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace fileserver.Services
+{
+    public class Authentication
+    {
+        private readonly DynamoDBContext _dynamoDbContext;
+        private readonly AmazonDynamoDBClient _amazonDynamoDBClient;
+        private readonly IAmazonCognitoIdentityProvider _cognitoClient;
+        private readonly string _userPoolId = "us-east-1_z1hTd7Wi7";
+        private readonly string _clientId = "tj39nh2d1v563du99b6pllk23";
+        private readonly string _clientSecret = "1q807qber9cpm1blu782jndeo6fg7mck6ncrpdbblq9c31bib6jk";
+        private readonly RegionEndpoint _region;
+
+        public Authentication()
+        {
+            _amazonDynamoDBClient = new AmazonDynamoDBClient();
+            var region = RegionEndpoint.USEast1; // e.g., RegionEndpoint.USWest2
+            _cognitoClient = new AmazonCognitoIdentityProviderClient(region);
+        }
+
+        public async Task<APIGatewayHttpApiV2ProxyResponse> AuthenticationFunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+        {
+            string httpMethod = request.RequestContext.Http.Method.ToUpper();
+
+            if (httpMethod == "OPTIONS")
+            {
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    StatusCode = 200,
+                    Headers = new Dictionary<string, string>
+                {
+                    { "Access-Control-Allow-Origin", "http://localhost:3000" },
+                    { "Access-Control-Allow-Headers", "Content-Type" },
+                    { "Access-Control-Allow-Methods", "OPTIONS,POST,GET" },
+                    { "Access-Control-Allow-Credentials", "true" },
+                },
+                };
+            }
+
+
+            if (httpMethod == "POST" && request.Body != null && request.PathParameters == null)
+            {
+                return await HandleUserRegistrationRequest(request);
+            }
+            else if (httpMethod == "GET" && request.Body == null && request.PathParameters != null)
+            {
+               // return await VerifyUser(request);
+            }
+
+            // Handle unsupported or unrecognized HTTP methods
+            return new APIGatewayHttpApiV2ProxyResponse { StatusCode = 400 };
+        }
+
+        // Register User
+        private async Task<APIGatewayHttpApiV2ProxyResponse> HandleUserRegistrationRequest(
+          APIGatewayHttpApiV2ProxyRequest request)
+        {
+            var userDto = System.Text.Json.JsonSerializer.Deserialize<UserAuthenticationDTO>(request.Body);
+            var document = new Document();
+            try
+            {
+                var provider = new AmazonCognitoIdentityProviderClient(
+                new BasicAWSCredentials(_clientId, _clientSecret), _region);
+                
+                // If there us a client secret, it should also be added here brfor provider
+                var pool = new CognitoUserPool(_userPoolId, _clientId, provider);
+               var userAttributes = new Dictionary<string, string>
+                {
+                    { "email", userDto.UserId },
+                    // Add other user attributes as needed
+                };
+
+                try
+                {
+                    await pool.SignUpAsync(userDto.UserId, userDto.Password, userAttributes, null);
+                    Console.WriteLine("User registered successfully.");
+                }
+                catch (Exception ex)
+                {
+                    return new APIGatewayHttpApiV2ProxyResponse()
+                    {
+                        Body = ex.Message,
+                        StatusCode = 400
+                    };
+                }
+                    // Save User in DynamoDB
+                    document["UserId"] = userDto.UserId;
+                    document["Password"] = userDto.Password;
+                    document["Verified"] = "0";
+
+                var table = Table.LoadTable(_amazonDynamoDBClient, "User");
+                var res = table.PutItemAsync(document);
+                if (res != null)
+                {
+                    return OkResponse();
+                }
+                return BadResponse("User was not registered !!!");
+            }
+            catch (Exception ex)
+            {
+                return BadResponse("User was not registered !!! " + ex.Message);
+            }
+        }
+
+        
+
+        // Sign In User
+        private async Task<APIGatewayHttpApiV2ProxyResponse> SignInUser(
+         APIGatewayHttpApiV2ProxyRequest request)
+        {
+            try
+            {
+                var userDto = System.Text.Json.JsonSerializer.Deserialize<SignInDTO>(request.Body);
+
+                var provider = new AmazonCognitoIdentityProviderClient(new AmazonCognitoIdentityProviderConfig
+                {
+                    RegionEndpoint = _region
+                });
+
+                var authenticationRequest = new InitiateAuthRequest
+                {
+                    AuthFlow = AuthFlowType.USER_PASSWORD_AUTH,
+                    ClientId = _clientId,
+                    AuthParameters = new Dictionary<string, string>
+                {
+                    { "USERNAME", userDto.UserName },
+                    { "PASSWORD", userDto.Password }
+                }
+                };
+
+                var authenticationResponse = await provider.InitiateAuthAsync(authenticationRequest);
+
+                if (authenticationResponse.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var accessToken = authenticationResponse.AuthenticationResult.AccessToken;
+                    var refreshToken = authenticationResponse.AuthenticationResult.RefreshToken;
+
+                    return new APIGatewayHttpApiV2ProxyResponse
+                    {
+                        StatusCode = 200,
+                        Body = $"Sign-in successful. Access Token: {accessToken}, Refresh Token: {refreshToken}",
+                    };
+                }
+                else
+                {
+                    return new APIGatewayHttpApiV2ProxyResponse
+                    {
+                        StatusCode = 400,
+                        Body = "Sign-in failed.",
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    StatusCode = 500,
+                    Body = "Error signing in: " + ex.Message,
+                };
+            }
+        }
+
+
+        // OK Response
+        private static APIGatewayHttpApiV2ProxyResponse OkResponse() =>
+            new APIGatewayHttpApiV2ProxyResponse()
+            {
+                StatusCode = 200
+            };
+
+        // Bad Response
+        private static APIGatewayHttpApiV2ProxyResponse BadResponse(string message)
+        {
+            return new APIGatewayHttpApiV2ProxyResponse()
+            {
+                Body = message,
+                StatusCode = 404
+            };
+        }
+    }
+}
---
+++ fileserver/Services/VerificationService.cs
@@ -0,0 +1,156 @@
+﻿using Amazon.CognitoIdentityProvider;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.CognitoIdentityProvider.Model;
+using fileserver.DTO;
+using Amazon.DynamoDBv2.Model;
+
+namespace fileserver.Services
+{
+    public class VerificationService
+    {
+        private readonly DynamoDBContext _dynamoDbContext;
+        private readonly AmazonDynamoDBClient _amazonDynamoDBClient;
+        private readonly IAmazonCognitoIdentityProvider _cognitoClient;
+        private readonly string _userPoolId = "us-east-1_z1hTd7Wi7";
+        private readonly string _clientId = "tj39nh2d1v563du99b6pllk23";
+        private readonly string _clientSecret = "1q807qber9cpm1blu782jndeo6fg7mck6ncrpdbblq9c31bib6jk";
+        private readonly RegionEndpoint _region;
+
+        public VerificationService()
+        {
+            _amazonDynamoDBClient = new AmazonDynamoDBClient();
+            var region = RegionEndpoint.USEast1; // e.g., RegionEndpoint.USWest2
+            _cognitoClient = new AmazonCognitoIdentityProviderClient(region);
+        }
+
+        public async Task<APIGatewayHttpApiV2ProxyResponse> VerificationFunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+        {
+            string httpMethod = request.RequestContext.Http.Method.ToUpper();
+
+            if (httpMethod == "OPTIONS")
+            {
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    StatusCode = 200,
+                    Headers = new Dictionary<string, string>
+                {
+                    { "Access-Control-Allow-Origin", "http://localhost:3000" },
+                    { "Access-Control-Allow-Headers", "Content-Type" },
+                    { "Access-Control-Allow-Methods", "OPTIONS,POST,GET" },
+                    { "Access-Control-Allow-Credentials", "true" },
+                },
+                };
+            }
+
+
+             if (httpMethod == "POST" && request.Body != null && request.PathParameters == null)
+            {
+                 return await VerifyUser(request);
+            }
+
+            // Handle unsupported or unrecognized HTTP methods
+            return new APIGatewayHttpApiV2ProxyResponse { StatusCode = 400 };
+        }
+
+        // Verify the Email Address
+        private async Task<APIGatewayHttpApiV2ProxyResponse> VerifyUser(
+         APIGatewayHttpApiV2ProxyRequest request)
+        {
+            try
+            {
+                var userDto = System.Text.Json.JsonSerializer.Deserialize<ConfirmEmailDTO>(request.Body);
+
+                var provider = new AmazonCognitoIdentityProviderClient(new AmazonCognitoIdentityProviderConfig
+                {
+                    RegionEndpoint = _region
+                });
+
+                var confirmRequest = new ConfirmSignUpRequest
+                {
+                    ClientId = _clientId,
+                    Username = userDto.Email,
+                    ConfirmationCode = userDto.Code
+                };
+                var confirmResponse = await provider.ConfirmSignUpAsync(confirmRequest);
+                // Check if confirmation was successful
+                if (confirmResponse.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    // User registration confirmed successfully. Now, obtain tokens.
+                    var authenticationRequest = new InitiateAuthRequest
+                    {
+                        AuthFlow = AuthFlowType.USER_PASSWORD_AUTH,
+                        ClientId = _clientId,
+                        AuthParameters = new Dictionary<string, string>
+                    {
+                        { "USERNAME", userDto.Email },
+                        { "PASSWORD", userDto.Password }, // Replace with the user's password
+                    }
+                    };
+                    var authenticationResponse = await provider.InitiateAuthAsync(authenticationRequest);
+
+                    // Check if authentication was successful
+                    if (authenticationResponse.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var updateRequest = new UpdateItemRequest
+                        {
+                            TableName = "User",
+                            Key = new Dictionary<string, AttributeValue>
+                    {
+                        { "UserId", new AttributeValue { S = userDto.Email } }
+                    },
+                            UpdateExpression = "SET Verified = :verified",
+                            ExpressionAttributeValues = new Dictionary<string, AttributeValue>
+                    {
+                        { ":verified", new AttributeValue { S = "1" } }
+                    }
+                        };
+                        // Perform the update operation
+                        await _amazonDynamoDBClient.UpdateItemAsync(updateRequest);
+
+                        var accessToken = authenticationResponse.AuthenticationResult.AccessToken;
+                        var refreshToken = authenticationResponse.AuthenticationResult.RefreshToken;
+
+                        return new APIGatewayHttpApiV2ProxyResponse
+                        {
+                            StatusCode = 200,
+                            Body = System.Text.Json.JsonSerializer.Serialize($"User registration confirmed successfully. Access_Token: {accessToken}, Refresh_Token: {refreshToken}"),
+                        };
+                    }
+                    else
+                    {
+                        return new APIGatewayHttpApiV2ProxyResponse
+                        {
+                            StatusCode = 400,
+                            Body = "User authentication failed.",
+                        };
+                    }
+                }
+                else
+                {
+                    return new APIGatewayHttpApiV2ProxyResponse
+                    {
+                        StatusCode = 400,
+                        Body = "User registration confirmation failed.",
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    Body = ex.Message,
+                    StatusCode = 500
+                };
+            }
+        }
+    }
+}
